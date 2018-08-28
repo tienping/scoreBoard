df9c29c0cdd230b09260a343ecb6d2ed5130cb2f
TienPing <petwesley@gmail.com>
2018-08-29 00:33:06 +0800

    add new components

---
+++ web/app/components/MyTableCell/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MyTableCell = styled.span`
+    display: inline-block;
+    padding: 1rem;
+    height: 3rem;
+    width: 5rem;
+    margin: 2px;
+    background: white;
+    alignSelf: center;
+`;
+
+export default MyTableCell;
