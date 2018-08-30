266a0ce5d9f4dd8bfb45f2745b4e12126470577d
TienPing <petwesley@gmail.com>
2018-08-30 11:37:59 +0800

    code clean up

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -28,20 +28,17 @@ const AppWrapper = styled.div`
 
 export default function App() {
   return (
-      <AppWrapper>
-          <Helmet
-            titleTemplate="%s - React.js Boilerplate"
-            defaultTitle="React.js Boilerplate"
-          >
-              <meta name="description" content="A React.js Boilerplate application" />
-          </Helmet>
-          <Header />
-          <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route path="/edit" component={EditPage} />
-              <Route path="" component={NotFoundPage} />
-          </Switch>
-          <Footer />
-      </AppWrapper>
+    <AppWrapper>
+      <Helmet titleTemplate="%s - ScoreBoard" defaultTitle="ScoreBoard">
+        <meta name="description" content="A React.js Boilerplate application" />
+      </Helmet>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/edit" component={EditPage} />
+        <Route path="" component={NotFoundPage} />
+      </Switch>
+      <Footer />
+    </AppWrapper>
   );
 }
